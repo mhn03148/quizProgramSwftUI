7d6685c8268e713d542b9a5c7e47e317ac9ebb0f
jujinhyeong <mhn03148@naver.com>
2023-06-21 14:25:56 +0900

    Initial Commit

---
+++ quizProgramSwftUI/quizProgramSwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizProgramSwftUIApp.swift
+//  quizProgramSwftUI
+//
+//  Created by 주진형 on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct quizProgramSwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
